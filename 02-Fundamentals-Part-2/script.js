f024f001a2dbd2da946114fb1480df262531b5d5
aiflyme <aiflyme@gmail.com>
2024-07-07 17:09:56 +0800

    Ch02 array && object

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -104,3 +104,62 @@ console.log(friends);
 friends.pop();
 friends.shift();
 console.log(friends);
+
+//Challenge 02#
+const calcTip = function (bill) {
+  if (bill >= 50 && bill <= 300) {
+    bill = bill * 0.15;
+  } else {
+    bill = bill * 0.2;
+  }
+  return bill;
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
+
+//Object
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Chen",
+  birthYear: 1983,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+  calcAge: function () {
+    return 2037 - this.birthYear;
+  },
+};
+console.log(jonas["job"]);
+jonas.sex = "male";
+console.log(jonas);
+
+console.log(jonas.calcAge());
+console.log(jonas["calcAge"]());
+
+//Challenge #03
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    return this.mass / this.height ** 2;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    return this.mass / this.height ** 2;
+  },
+};
+console.log(mark.calcBMI(), john.calcBMI());
+console.log(
+  `${mark.fullName}'s BMI ${mark.calcBMI().toFixed(2)} is higher than ${
+    john.fullName
+  }'s ${john.calcBMI().toFixed(2)}`
+);
