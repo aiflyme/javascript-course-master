9e87559c04f6b1648e9fa6d443dae3a389687cb4
Robin Chen <aiflyme@gmail.com>
2024-07-10 22:28:50 +0800

    practice ch04

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const x = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.check').addEventListener('click', () => {
+  //get use input number
+  const guess = document.querySelector('.guess').value;
+
+  const textContent = document.querySelector('.score').textContent;
+  //textContent = Number(textContent) - 1;
+  document.querySelector('.score').textContent = Number(textContent) - 1;
+
+  //if statement
+  if (!guess) {
+    document.querySelector('.message').textContent = '🪨 No number!';
+  } else if (Number(guess) === x) {
+    document.querySelector('.message').textContent = 'Correct numer!';
+    document.querySelector('.highscore').textContent =
+      document.querySelector('.score').textContent;
+  } else if (Number(guess) > x) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (Number(guess) < x) {
+    document.querySelector('.message').textContent = 'Too low!';
+  }
+});
+
+//score
+//highscore
