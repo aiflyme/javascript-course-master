c24bfe4fcf19587a5328a9173e2aa54ce1d3d46a
aiflyme <aiflyme@gmail.com>
2024-09-02 15:56:45 +0800

    CH10 functions 131

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,125 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  fightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    fightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
+
+//129
+const flight = 'LH234';
+const jonas = {
+  name: 'Robin Chen',
+  passport: 8956485693,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr ' + passenger.name;
+
+  if (passenger.passport === 8956485693) {
+    console.log('Check in');
+  } else {
+    console.log('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight, jonas);
+
+let flightNum = flight;
+const passenger = jonas;
+flightNum = 'LH888';
+console.log(flightNum, flight, passenger);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+//131
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('five');
+};
+// document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+//132
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Robin');
+
+greet('hello')('ivy');
+
+//arrow
+const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArrow('hello')('ray');
+
+//133 the call and apply method
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(236, 'Robin Chen');
+console.log(lufthansa.bookings);
+const eurowings = {
+  name: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
