46d86b5b9bfe45037e6b6331e8f7a64bb6985725
aiflyme <aiflyme@gmail.com>
2024-09-09 16:55:11 +0800

    CH11 142 simple array methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,35 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+//142 Simple Array Methods
+console.log('### 142 Simple Array Methods');
+
+//slice
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2));
+console.log(arr.slice(1, 3));
+console.log(arr.slice(-3, -1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log(...arr);
+
+//splice     delete from arr
+console.log(arr.splice(1, 2));
+arr.splice(-1);
+console.log(arr);
+//reverse
+let arr2 = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr2.reverse());
+console.log(arr2);
+
+//concat
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//join
+console.log(letters.join(' - '));
