dbcee1cbba2f90e00ee13e302846e5ff46a28dc9
aiflyme <aiflyme@gmail.com>
2024-09-13 15:49:32 +0800

    finish 157

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -226,3 +226,170 @@ const reduceEx = [3, 5, 2, 12, 7].reduce(function (prev, cur, index, arr) {
   return prev + cur;
 }, 11); //11 is initial value
 console.log(reduceEx);
+
+//150 The map method
+const movements150 = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+// const movements150USD = movements150.map(function (mov) {
+//   return (mov *= eurToUsd);
+// });
+const movements150USD = movements150.map(mov => mov * eurToUsd);
+console.log(movements150);
+console.log(movements150USD);
+
+const movementsDesc150 = movements150.map(function (v, i, arr) {
+  if (v > 0) {
+    return `Movement ${i + 1}: You deposited ${v}`;
+  } else {
+    return `Movement ${i + 1}: You withdrew ${Math.abs(v)}`;
+  }
+});
+console.log(movementsDesc150);
+
+//151 Computering usernames
+const user = 'Steven Thomas Williams'; //stw
+const createUsername = users => {
+  users.forEach(user => {
+    user.username = user.owner
+      .toLowerCase()
+      .split(' ')
+      .map(v => v[0])
+      .join('');
+  });
+  //return users;
+};
+createUsername(accounts);
+console.log(accounts);
+
+//152 filter
+//test with map return true or false
+const depositsMap = movements.map(function (value) {
+  return value > 0;
+});
+
+const deposits = movements.filter(function (value) {
+  return value > 0;
+});
+console.log(depositsMap);
+console.log(deposits);
+const withdrawals = movements.filter(value => value < 0);
+console.log(withdrawals);
+
+//153 reduce
+const balance = movements.reduce((acc, cur, i, arr) => {
+  console.log(acc, cur);
+  return (acc += cur);
+}, -5000);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+const calcPrintBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} €`;
+};
+
+calcPrintBalance(account1.movements);
+
+//Maximum value
+let maxNum = 0;
+const max = movements.reduce((acc, cur) => (acc > cur ? acc : cur), 0);
+console.log(max);
+
+//154 Challenge #2
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = function (data) {
+  const arr = [];
+
+  const humanAge = data.map(
+    v => (v <= 2 ? 2 * v : 16 + v * 4)
+    //arr.push(humanAge);
+  );
+
+  //console.log(humanAge);
+  const adults = humanAge.filter(v => {
+    return v >= 18;
+  });
+  // const avgDogsAge = adults.reduce((acc, v) => acc + v, 0) / adults.length;
+  console.log(adults);
+  const avgDogsAge = adults.reduce((acc, v, i, arr) => acc + v / arr.length, 0);
+
+  return avgDogsAge;
+};
+
+const calcAverageHumanAgePipeline = data =>
+  data
+    .map(v => (v <= 2 ? 2 * v : 16 + v * 4))
+    .filter(v => v >= 18)
+    .reduce((acc, v, i, arr) => acc + v / arr.length, 0);
+
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+const data2 = [16, 6, 10, 5, 6, 1, 4];
+const dataRs1 = calcAverageHumanAge(data1);
+const dataRs2 = calcAverageHumanAge(data2);
+const dataPipeRs1 = calcAverageHumanAgePipeline(data1);
+const dataPipeRs2 = calcAverageHumanAgePipeline(data2);
+console.log(dataRs1, dataRs2);
+console.log(dataPipeRs1, dataPipeRs2);
+
+//155 The Magic of Chaining Methods
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = incomes + ` €`;
+
+  const outcomes = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = Math.abs(outcomes) + `€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, interest) => acc + interest, 0);
+  labelSumInterest.textContent = interest + `€`;
+};
+calcDisplaySummary(account1.movements);
+
+//156 Challenge #3
+const calcAverageHumanAgePipelines = data =>
+  data
+    .map(v => (v <= 2 ? 2 * v : 16 + v * 4))
+    .filter(v => v >= 18)
+    .reduce((acc, v, i, arr) => acc + v / arr.length, 0);
+
+//157 the find method
+const findRs = movements.find(mov => mov < 0);
+console.log(findRs); // one element
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+
+//158 Implementing login
